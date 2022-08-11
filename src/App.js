802f8afc2dc40785f7eaeeb37d059c666382970f
Brendan O'Connor <bhoconnor@gmail.com>
2022-08-11 07:54:20 -0400

    Change app.js to arrow functions & add reportWebVitals.js & yarn.lock files

--- src/App.js
+++ src/App.js
@@ -1,62 +1,72 @@
-import * as React from "react";
+import * as React from 'react';
 
 const list = [
   {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    topic: "Reactionaries",
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    topic: 'Reactionaries',
     num_comments: 3,
     points: 4,
     objectID: 0,
   },
   {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    topic: "Sentaries",
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    topic: 'Sentaries',
     num_comments: 2,
     points: 5,
     objectID: 1,
   },
   {
-    title: "Flummoxed",
-    url: "https://life.js.org/",
+    title: 'Flummoxed',
+    url: 'https://life.js.org/',
     author: "Brendan O'Connor",
-    topic: "Life decisionaries",
+    topic: 'Life decisionaries',
     num_comments: 17,
     points: 5,
     objectID: 2,
   },
 ];
 
-function App() {
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+    <Search />
 
-      <hr />
+    <hr />
 
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.topic}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
-}
+    <List />
+
+    <List />
+
+    <List />
+  </div>
+);
+
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div>
+);
+
+const List = () => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.topic}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
+  </ul>
+);
 
 export default App;
